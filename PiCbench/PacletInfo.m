b6005fc406c083d7c31559c52b31ea4b6b4b1360
Guillermo Hernandez <guillehg@gmail.com>
2015-12-02 17:04:40 +0100

    First commit.

---
+++ PiCbench/PacletInfo.m
@@ -0,0 +1,16 @@
+(* Paclet Info File *)
+
+(* created 2015/12/02*)
+
+Paclet[
+    Name -> "PiCbench",
+    Version -> "0.0.1",
+    MathematicaVersion -> "6+",
+    Creator -> "Guillermo Hernandez",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
---
+++ PiCbench/PiCbench/PiCbench.m
@@ -0,0 +1,16 @@
+(* Mathematica Package *)
+
+(* Created by the Wolfram Workbench 02-dic-2015 *)
+
+BeginPackage["PiCbench`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
