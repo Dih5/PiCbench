bbc3b0a725223c065ad9fe088c9cec0c97069841
guillehg <guillehg@gmail.com>
2016-01-08 12:13:35 +0100

    Added Plot.

---
+++ PiCbench/PiCbench/Plot.m
@@ -0,0 +1,10 @@
+(* Mathematica Package *)
+
+BeginPackage["PiCbench`Plot`"]
+(* Exported symbols added here with SymbolName::usage *)  
+
+Begin["`Private`"] (* Begin Private Context *) 
+
+End[] (* End Private Context *)
+
+EndPackage[]
