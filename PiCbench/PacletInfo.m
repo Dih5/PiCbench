94f9696994cf891d9d59af49d38f703401cfa3c4
guillehg <guillehg@gmail.com>
2016-01-08 13:10:30 +0100

    Updated PacletInfo.

--- PiCbench/PacletInfo.m
+++ PiCbench/PacletInfo.m
@@ -1,11 +1,12 @@
 (* Paclet Info File *)
 
-(* created 2015/12/02*)
+(* created 2016/01/08*)
 
 Paclet[
     Name -> "PiCbench",
     Version -> "0.0.1",
-    MathematicaVersion -> "6+",
+    MathematicaVersion -> "9+",
+    Description -> "Tools for making Particle in Cell simulations in Mathematica.",
     Creator -> "Guillermo Hernandez",
     Extensions -> 
         {
