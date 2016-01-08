e5e431194c01461ff4b69a02c80ad3e1621f91c9
guillehg <guillehg@gmail.com>
2016-01-08 15:43:51 +0100

    Added compile functionality.

--- PiCbench/PiCbench/Compile.m
+++ PiCbench/PiCbench/Compile.m
@@ -1,10 +1,67 @@
 (* Mathematica Package *)
 
 BeginPackage["PiCbench`Compile`"]
-(* Exported symbols added here with SymbolName::usage *)  
+RemoveIdentities::usage="RemoveIdentities is a boolean option for FCompile which sets whether some elementary algebraic simplifications are performed."
+FCompile::usage="FCompile(f,types,ranks, addedAsumptions) compiles the function f assuming arguments have given types and ranks."
+BenchmarkFCompile::usage="BenchmarkFCompile({f,types,ranks, addedAsumptions},args,times) bencharmks different compilation methods returning the mean AbsoluteTiming of calling the compiled function with args the given times."
+
 
 Begin["`Private`"] (* Begin Private Context *) 
 
+Options[FCompile] = Union[Options[Compile], {RemoveIdentities -> True}]
+
+(*Some algebraic simplifications. To check their effect one may use CompilePrint*)
+IdentityRules = {HoldPattern[
+     Times[x_, a_, Power[Times[a_, y_], -1]]] :> x/y, 
+   HoldPattern[Times[a_, Power[Times[a_, y_], -1]]] :> 1/y, 
+   HoldPattern[Times[x_, a_, Power[a_, -1]]] :> x, 
+   HoldPattern[Times[a_, Power[a_, -1]]] :> 1, 
+   HoldPattern[Times[a_, 1.]] :> a, HoldPattern[Times[a_, 1]] :> a, 
+   HoldPattern[x_ + 0.] :> x, HoldPattern[x_ + 0] :> x};
+   
+FCompile[f_Function, types_List, ranks_List, addedAsumptions_List: {},
+   opts : OptionsPattern[]] := Block[{fTemp},
+  fTemp = 
+   Hold @@ If[OptionValue[RemoveIdentities] === True, 
+     ReplaceRepeated[f, IdentityRules], f];
+  fTemp[[1]] = Join[MapThread[List, {f[[1]], types, ranks}]];
+  Compile @@ 
+   Join[fTemp, Hold[addedAsumptions], 
+    Hold @@ FilterRules[{opts}, Options[Compile]]]
+  ]
+
+(*Modes of compilation used by the bencharmk function*)
+FCompileFlavours[f_Function, types_List, ranks_List, 
+  addedAsumptions_List: {}] := 
+ Block[{comp, compC, compCSpeed, compCSpeedNoSimp},
+  comp = FCompile[f, types, ranks, addedAsumptions];
+  compC = 
+   FCompile[f, types, ranks, addedAsumptions, 
+    CompilationTarget -> "C"];
+  compCSpeed = 
+   FCompile[f, types, ranks, addedAsumptions, 
+    CompilationTarget -> "C", RuntimeOptions -> "Speed"];
+  compCSpeedNoSimp = 
+   FCompile[f, types, ranks, addedAsumptions, 
+    CompilationTarget -> "C", RuntimeOptions -> "Speed", 
+    RemoveIdentities -> False];
+  {f, comp, compC, compCSpeed, compCSpeedNoSimp}]
+  
+BenchmarkFCompile[{f_Function, types_List, ranks_List, 
+   addedAsumptions_List: {}}, args_List, times_Integer] := 
+ Block[{comp, compC, compCSpeed, compCSpeedNoSimp},
+  {comp, compC, compCSpeed, compCSpeedNoSimp} = 
+   Drop[FCompileFlavours[f, types, ranks], 1];
+  {(Table[f @@ args ;, {times}]; // AbsoluteTiming // First)/times,
+   (Table[comp @@ args; , {times}]; // AbsoluteTiming // First)/
+    times,
+   (Table[compC @@ args; , {times}]; // AbsoluteTiming // First)/times,
+   (Table[compCSpeed @@ args; , {times}]; // AbsoluteTiming // First)/
+    times,
+   (Table[compCSpeedNoSimp @@ args; , {times}]; // AbsoluteTiming // 
+      First)/times
+   }]
+   
 End[] (* End Private Context *)
 
 EndPackage[]
