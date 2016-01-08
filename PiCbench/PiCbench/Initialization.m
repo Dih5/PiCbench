fcfa3201e96c92a707cdce5d026e20203c2cb69a
guillehg <guillehg@gmail.com>
2016-01-08 11:26:39 +0100

    Added Initialization.m

---
+++ PiCbench/PiCbench/Initialization.m
@@ -0,0 +1,10 @@
+(* Mathematica Package *)
+
+BeginPackage["PiCbench`Initialization`"]
+(* Exported symbols added here with SymbolName::usage *)  
+
+Begin["`Private`"] (* Begin Private Context *) 
+
+End[] (* End Private Context *)
+
+EndPackage[]
