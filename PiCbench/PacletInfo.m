4e5739c811b0559e5042f837def3a5855e05cb46
guillehg <guillehg@gmail.com>
2016-01-11 16:43:26 +0100

    Added empty documentation pages.

--- PiCbench/PacletInfo.m
+++ PiCbench/PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2016/01/08*)
+(* created 2016/01/11*)
 
 Paclet[
     Name -> "PiCbench",
-    Version -> "0.0.1",
+    Version -> "0.1.0",
     MathematicaVersion -> "9+",
     Description -> "Tools for making Particle in Cell simulations in Mathematica.",
     Creator -> "Guillermo Hernandez",
